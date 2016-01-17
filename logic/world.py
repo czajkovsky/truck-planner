09656215ffc96ebd9fc86352a7c46aa381552720
Mateusz Czajka <mat.czajka@gmail.com>
2016-01-17 14:28:01 +0100

    add distance limit

--- logic/world.py
+++ logic/world.py
@@ -12,33 +12,26 @@ class World:
     sites = range(len(self.citiesNames))
     clients = sites[1:]
 
-    truckNames = ['Transit', 'TIR']
-    truckCapacities = [10, 20]
-    truckRates = [1.1, 2.0]
-    truckLimits = [4, 2]
-    trucks = range(len(truckNames))
+    truckNames = ['Transit#1', 'Transit#2', 'Transit#3', 'Transit#4', 'TIR#1', 'TIR#2']
+    truckMaxDeliveryPoints = [20, 20, 20, 20, 3, 3]
+    truckMaxDailyDistance = [200, 200, 200, 200, 500, 500]
+    truckCapacities = [10, 10, 10, 10, 20, 20]
+    truckRates = [1.1, 1.1, 1.1, 1.1, 2.0, 2.0]
 
-    vehicleNames = ['Transit#1', 'Transit#2', 'Transit#3', 'Transit#4', 'TIR#1', 'TIR#2']
-    vehicleMaxDeliveryPoints = [20, 20, 20, 20, 3, 3]
-    vehicleCapacities = [10, 10, 10, 10, 20, 20]
-    vehicleRates = [1.1, 1.1, 1.1, 1.1, 2.0, 2.0]
-    vehicles = range(len(vehicleNames))
+    trucks = range(len(truckNames))
 
     capacity = 20
 
     model = Model('Diesel Fuel Delivery')
 
     x = {} # communication from i -> j
-    # t = {} # truck ti on route i -> j t[i, j, ti]
-    v = {} # vehicle v on route i->j v[t,j,ti]
+    t = {} # truck t on route i->j t[t,j,ti]
 
     for i in sites:
       for j in sites:
         x[i, j] = model.addVar(vtype = GRB.BINARY)
-        # for ti in trucks:
-        #   t[i, j, ti] = model.addVar(vtype = GRB.BINARY)
-        for vi in vehicles:
-          v[i, j, vi] = model.addVar(vtype = GRB.BINARY)
+        for ti in trucks:
+          t[i, j, ti] = model.addVar(vtype = GRB.BINARY)
 
     u = {}
     for i in clients:
@@ -47,7 +40,7 @@ class World:
     model.update()
 
     obj = quicksum(
-      self.distances[i][j] * x[i,j] * v[i, j, vi] * vehicleRates[vi] for i in sites for j in sites for vi in vehicles if i != j
+      self.distances[i][j] * x[i,j] * t[i, j, ti] * truckRates[ti] for i in sites for j in sites for ti in trucks if i != j
     )
 
     model.setObjective(obj)
@@ -60,15 +53,14 @@ class World:
 
     for i in sites:
       for j in sites:
-        # model.addConstr(quicksum(t[i, j, ti] for ti in trucks if i != j) == x[i,j])
-        model.addConstr(quicksum(v[i, j, vi] for vi in vehicles if i != j) == x[i,j])
+        model.addConstr(quicksum(t[i, j, ti] for ti in trucks if i != j) == x[i,j])
 
     for i in clients:
-      c = quicksum(v[j, i, vi] * vehicleCapacities[vi] for vi in vehicles for j in sites if i != j)
+      c = quicksum(t[j, i, ti] * truckCapacities[ti] for ti in trucks for j in sites if i != j)
       model.addConstr(u[i] <= c + (self.demands[i - 1] - c) * x[0, i])
 
     for i in clients:
-      c = quicksum(v[k, i, vi] * vehicleCapacities[vi] for vi in vehicles for k in sites if i != k)
+      c = quicksum(t[k, i, ti] * truckCapacities[ti] for ti in trucks for k in sites if i != k)
       for j in clients:
         if i != j:
           model.addConstr(u[i] - u[j] + c * x[i, j] <= c - self.demands[j - 1])
@@ -77,41 +69,29 @@ class World:
       for routeIn in sites:
         for routeOut in sites:
           if (i != routeIn) & (i != routeOut):
-            # for ti in trucks:
-            #   model.addConstr(t[i,routeOut,ti] * x[routeIn,i] == t[routeIn,i,ti] * x[i,routeOut])
-            for vi in vehicles:
-              model.addConstr(v[i,routeOut,vi] * x[routeIn,i] == v[routeIn,i,vi] * x[i,routeOut])
+            for ti in trucks:
+              model.addConstr(t[i,routeOut,ti] * x[routeIn,i] == t[routeIn,i,ti] * x[i,routeOut])
 
-    # for ti in trucks:
-    #   model.addConstr(quicksum(t[0, i, ti] for i in clients) <= truckLimits[ti])
+    for ti in trucks:
+      model.addConstr(quicksum(t[0, i, ti] for i in clients) <= 1)
 
-    for vi in vehicles:
-      model.addConstr(quicksum(v[0, i, vi] for i in clients) <= 1)
-
-
-    for vi in vehicles:
-      model.addConstr(quicksum(v[i, j, vi] for i in clients for j in clients if j != i) <= vehicleMaxDeliveryPoints[vi])
+    for ti in trucks:
+      model.addConstr(quicksum(t[i, j, ti] * x[i,j] * self.distances[i][j] for i in sites for j in sites if j != i) <= truckMaxDailyDistance[ti])
+      model.addConstr(quicksum(t[i, j, ti] for i in clients for j in clients if j != i) <= truckMaxDeliveryPoints[ti])
 
     model.optimize()
-
-    for i in sites:
-      for j in sites:
-        if i != j:
-          for vi in vehicles:
-            if v[i, j, vi].X > 0.5:
-              print self.citiesNames[i], '->', self.citiesNames[j], ':', vehicleNames[vi]
 
     def printTour(start, visited, distance):
       sys.stdout.write(self.citiesNames[start] + ' -> ')
       for i in sites:
         if (x[start, i].X > 0.5) & (start != i):
           if (i == 0):
-            vehicleId = -1
-            for vi in vehicles:
-              if (v[start,i,vi].X > 0.5):
-                vehicleId = vi
+            truckId = -1
+            for ti in trucks:
+              if (t[start,i,ti].X > 0.5):
+                truckId = ti
             totalDistance = distance + self.distances[start][i]
-            print 'FACTORY, distance:', totalDistance, 'TRUCK:', vehicleNames[vehicleId], 'COST:', totalDistance * vehicleRates[vehicleId]
+            print 'FACTORY, distance:', totalDistance, 'TRUCK:', truckNames[truckId], 'COST:', totalDistance * truckRates[truckId]
             return
           printTour(i, visited, distance + self.distances[start][i])
 
